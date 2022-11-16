d890357cea32a859b7a5120d0261ec7fb4ce84ff
sc86 <sebascaro0806@gmail.com>
2022-11-16 11:48:56 -0500

    Added bearer basic authentication

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
+from fastapi.security import HTTPBearer
 from src.config.setup import settings
 
 user = APIRouter()
+security = HTTPBearer()
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     description=settings.PROJECT_DESCRIPTION,
@@ -11,9 +14,9 @@ app = FastAPI(
     openapi_url=settings.URL_PREFIX + '/openapi.json'
 )
 
-@user.get("/users", response_model=str)
-def get_users():
-    return "Hola Mundo despliegue"
+@user.get("/root", response_model=str, dependencies=[Depends(security)])
+def root():
+    return "Test " + settings.PROJECT_NAME
 
 app.include_router(user, prefix=settings.URL_PREFIX)
 
